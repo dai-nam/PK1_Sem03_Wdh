6e7120d7672df7cd533f7c65af0bb94a0947f5bc
dai-nam <58533766+dai-nam@users.noreply.github.com>
2019-12-22 18:35:03 +0100

    Fertig bis Praktikum 07

---
+++ Medienverwaltung_Wdh/src/EmptyFilenameException.java
@@ -0,0 +1,17 @@
+import javax.swing.JOptionPane;
+
+public class EmptyFilenameException extends Exception {
+
+	private int selected;
+
+	public EmptyFilenameException() {
+		super();
+		selected = JOptionPane.showConfirmDialog(null, "Leerer Dateiname. Erneut eingeben?", "Hinweis",
+				JOptionPane.OK_CANCEL_OPTION);
+	}
+
+	public int getSelected() {
+		return this.selected;
+	}
+
+}
---
+++ Medienverwaltung_Wdh/src/Medienliste.java
@@ -0,0 +1,18 @@
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Medienliste implements Serializable{
+
+	private static final long serialVersionUID = 9072727940676826777L;
+	List<Medium> medien;
+
+	public Medienliste() {
+		medien = new LinkedList<Medium>();
+	}
+
+	public List<Medium> getList() {
+		return this.medien;
+	}
+
+}
